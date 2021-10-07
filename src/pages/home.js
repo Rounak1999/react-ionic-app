1d069bb26ae69e6b14e3048fee47a7d150e0bb1b
Rounak Gour <rounakgour110@gmail.com>
2021-10-07 13:20:17 +0530

    ok

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,11 @@ import {
     IonCard,
     IonCardHeader,
     IonCardTitle,
-    IonContent
+    IonContent,
+    IonCardContent
     } from '@ionic/react';
     import React from 'react';
+    import GridExample from './aggrid';
     
     function Home() {
        return (
@@ -13,6 +15,9 @@ import {
            <IonCardHeader>
            <IonCardTitle>Welcome to Home Page</IonCardTitle>
            </IonCardHeader>
+           <IonCardContent>
+             <GridExample />
+           </IonCardContent>
           </IonCard>
          </IonContent>
        );
